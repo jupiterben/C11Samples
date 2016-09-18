450c0af764325bc5f020059f777507b247aee48f
jupiter <jupiter.ben@live.com>
2016-09-18 15:46:44 +0800

    add vtk sample

--- source/multiThread/sharedMutex.cpp
+++ source/multiThread/sharedMutex.cpp
@@ -4,6 +4,8 @@
 #include <map>
 #include <thread>
 
+
+
 class ThreadSafeMap
 {
 public:
@@ -14,20 +16,58 @@ public:
         if(iter!=m_map.end())
         {
             val = iter->second;
+            lock.unlock();
+            std::cout<<"get string " << nKey << " : " << val << std::endl;
             return true;
         } 
         return false;
     }
-
+    void setString(int nKey, const std::string& val)
+    {
+        std::lock_guard<boost::shared_mutex> lock(mutex_);
+        m_map[nKey] = val;     
+        std::cout<< "set string" << nKey << " : " << val << std::endl;
+    }
 protected:
     std::map<int , std::string> m_map;
     mutable boost::shared_mutex mutex_;
 };
 
+#define MAX_SIZE 1000
+void readThread(const ThreadSafeMap& m)
+{
+    for(int i=0;i<MAX_SIZE*100;i++)
+    {
+        int nrand = rand()%MAX_SIZE;
+        std::string s;
+        m.getString(nrand, s);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
+}
+void writeThread(ThreadSafeMap& m)
+{
+    for(int i=0;i<MAX_SIZE;i++)
+    {
+        std::string s = "h";
+        m.setString(i, s);
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
+
 int main()
 {
-    
+    ThreadSafeMap m;
 
+
+
+    std::thread threadWrite(writeThread, std::ref(m));
+    std::thread threadReader1(readThread, std::ref(m));
+    std::thread threadReader2(readThread, std::ref(m));
+
+
+    threadReader1.join();
+    threadReader2.join();
+    threadWrite.join();
 
     return 1;
 }
