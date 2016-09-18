e9f01d8e1975fdea704f151997ff5a06ed375373
JupiterBen <jupiter.ben@live.com>
2016-09-18 21:05:02 +0800

    add mutex for std::cout in sharedMutex sample

--- source/multiThread/sharedMutex.cpp
+++ source/multiThread/sharedMutex.cpp
@@ -1,23 +1,22 @@
 
 #include <boost/thread/shared_mutex.hpp>
+#include <boost/format.hpp>
 #include <string>
 #include <map>
 #include <thread>
 
-
+std::mutex coutMutex;
 
 class ThreadSafeMap
 {
 public:
     bool getString(int nKey, std::string& val)const
     {
-        boost::shared_lock<boost::shared_mutex> lock(mutex_);
+        boost::shared_lock<boost::shared_mutex> lock(mutex_); //many thread can enter here, except  
         auto iter = m_map.find(nKey);
         if(iter!=m_map.end())
         {
             val = iter->second;
-            lock.unlock();
-            std::cout<<"get string " << nKey << " : " << val << std::endl;
             return true;
         } 
         return false;
@@ -26,21 +25,26 @@ public:
     {
         std::lock_guard<boost::shared_mutex> lock(mutex_);
         m_map[nKey] = val;     
-        std::cout<< "set string" << nKey << " : " << val << std::endl;
     }
 protected:
     std::map<int , std::string> m_map;
     mutable boost::shared_mutex mutex_;
 };
 
-#define MAX_SIZE 1000
+#define MAX_SIZE 10
 void readThread(const ThreadSafeMap& m)
 {
-    for(int i=0;i<MAX_SIZE*100;i++)
+    for(int i=0;i<MAX_SIZE*10;i++)
     {
         int nrand = rand()%MAX_SIZE;
         std::string s;
-        m.getString(nrand, s);
+        if( m.getString(nrand, s) )
+        {    
+            std::lock_guard<std::mutex> lk(coutMutex);
+            boost::format fmtstr("thread %1% get %2% at %3%");
+            fmtstr % std::this_thread::get_id() % s % nrand;
+            std::cout<< fmtstr << std::endl;
+        }
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
     }
 }
@@ -50,6 +54,12 @@ void writeThread(ThreadSafeMap& m)
     {
         std::string s = "h";
         m.setString(i, s);
+        {
+            std::lock_guard<std::mutex> lk(coutMutex);
+            boost::format fmtstr("thread %1% set %2% at %3%");
+            fmtstr % std::this_thread::get_id() % s % i;
+            std::cout<< fmtstr << std::endl;
+        }
         std::this_thread::sleep_for(std::chrono::milliseconds(10));
     }
 }
@@ -57,8 +67,6 @@ void writeThread(ThreadSafeMap& m)
 int main()
 {
     ThreadSafeMap m;
-
-
 
     std::thread threadWrite(writeThread, std::ref(m));
     std::thread threadReader1(readThread, std::ref(m));
